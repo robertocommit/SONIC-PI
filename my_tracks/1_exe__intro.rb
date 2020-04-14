0821f58298b3f5688b6cdd19f9616dd7f8f17f7d
robimalco <roberto.malcotti@datangels.com>
2020-04-14 23:58:09 +0200

    Better random attributes

--- my_tracks/1_exe__intro.rb
+++ my_tracks/1_exe__intro.rb
@@ -1,6 +1,6 @@
 with_fx :echo, mix: 0.8 do
   live_loop :intro do
-    play (scale :C4, :minor_pentatonic), release: rrand(0.1, 4), decay: 0.1
+    play (scale :C4, :minor_pentatonic), release: rrand(0.1, 4), decay: rrand(0.1, 0.4), attack: rrand(0.1, 0.4), sustain: rrand(0.1, 0.4)
     play (scale :Bb, :major_pentatonic), amp: 0.2
     play (scale :D3, :minor_pentatonic), amp: 0.2
     sample :ambi_piano, attack: 0.5, attack_level: 1
@@ -10,7 +10,7 @@ end
 
 with_fx :reverb, mix: 0.8 do
   live_loop :intro_2 do
-    play (scale :C4, :minor_pentatonic), release: rrand(0.1, 3), decay: 0.6, decay_level: 1
+    play (scale :C4, :minor_pentatonic), release: rrand(0.1, 4), decay: rrand(0.1, 0.4), attack: rrand(0.1, 0.4), sustain: rrand(0.1, 0.4)
     play (scale :Bb, :major_pentatonic), amp: 0.2
     play (scale :D3, :minor_pentatonic), amp: 0.2
     sample :ambi_piano, attack: 0.1, attack_level: 1
