b07cd368b55040656475748e923fec0bf87ea90a
robimalco <roberto.malcotti@datangels.com>
2020-04-20 23:18:58 +0200

    Update peso.rb

--- my_tracks/peso.rb
+++ my_tracks/peso.rb
@@ -1,10 +1,16 @@
-5.times do
+define :bloob do
   use_synth :fm
   p = play (chord :Eb3, :minor).choose - [0, 12, -12].choose, divisor: 0.01, div_slide: rrand(0, 10), depth: rrand(0.001, 2), attack: 0.01, release: rrand(0, 5), amp: 0.5
   control p, divisor: rrand(0.001, 50)
   sleep [0.5, 1, 2].choose
 end
 
+
+5.times do
+  bloob
+end
+
+sleep 1
 live_loop :first_block do
   sample :elec_blup
   sample :bd_ada
@@ -14,7 +20,7 @@ end
 sleep 4
 live_loop :second_block do
   sleep 0.4
-  with_fx :distortion, distort: 0.09 do
+  with_fx :distortion, distort: [0.07, 0.09, 0.11, 0.13, 0.15].choose do
     sample :bd_haus
   end
 end
@@ -28,7 +34,7 @@ end
 sleep 8
 live_loop :fourth_block do
   sleep 4
-  sample :drum_snare_hard, rate: 0.1
+  sample :drum_snare_hard, rate: [0.1, 0.3, 0.5].choose
 end
 
 sleep 4
@@ -57,9 +63,9 @@ live_loop :seventh_block do
   sleep 16
   with_fx :reverb, room: 1 do
     with_fx :echo do
-      30.times do
+      rrand(25, 35).times do
         sample :elec_triangle, amp: 0.5, release: 5
-        sleep 0.02
+        sleep [0.01, 0.02, 0.03].choose
       end
     end
   end
@@ -67,8 +73,5 @@ end
 
 sleep 4
 live_loop :eigth_block do
-  use_synth :fm
-  p = play (chord :Eb3, :minor).choose - [0, 12, -12].choose, divisor: 0.01, div_slide: rrand(0, 10), depth: rrand(0.001, 2), attack: 0.01, release: rrand(0, 5), amp: 0.5
-  control p, divisor: rrand(0.001, 50)
-  sleep [0.5, 1, 2].choose
+  bloob
 end
